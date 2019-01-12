4ea7155d0ff818892d65c05282f186a09ef31923
laurent <ladardenne@orange.fr>
2019-01-12 14:48:37 +0100

    trim whitespace

--- Test/SuppressMessageBinaryExpressionAst.ps1
+++ Test/SuppressMessageBinaryExpressionAst.ps1
@@ -1,10 +1,10 @@
 ﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute("ForStatementCanBeImproved","")]
-     
+
 param()
- For($i=0; $i -lt ($Range.Count-1); $i++) 
+ For($i=0; $i -lt ($Range.Count-1); $i++)
  { $i }
- 
- For($i=0; $i -lt $Range.Count-1; $i++) 
+
+ For($i=0; $i -lt $Range.Count-1; $i++)
  { $i }
 
  $RangeCount = $Range.Count
